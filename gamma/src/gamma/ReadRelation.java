476a08035ce5acab788dc7bde654f3fe009c9f51
Katrina Van Laan <kdvanlaan@gmail.com>
2018-04-20 16:32:32 -0500

    first attempt at read relation

--- gamma/src/gamma/ReadRelation.java
+++ gamma/src/gamma/ReadRelation.java
@@ -5,10 +5,40 @@
  */
 package gamma;
 
+import java.io.*;
+import java.util.*;
+
 /**
  *
  * @author katrinavanlaan
  */
 public class ReadRelation {
-    
+
+    public static StringTokenizer readFile(String inputFile) {
+        try {
+            File file = new File(inputFile);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            StringBuffer stringBuffer = new StringBuffer();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuffer.append(line);
+                stringBuffer.append("\n");
+            }
+            fileReader.close();
+
+            StringTokenizer st = new StringTokenizer(stringBuffer.toString());
+            System.out.println("---- Split by space ------");
+            while (st.hasMoreElements()) {
+                System.out.println(st.nextElement());
+            }
+            return st;
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+        StringTokenizer st2 = new StringTokenizer("");
+        return st2;
+    }
+
 }
