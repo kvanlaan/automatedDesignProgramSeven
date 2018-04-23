546cfcffd84e7095030c49eb9c0ffa3f83ba6ae5
xinyuan <xinyuan.chn@gmail.com>
2018-04-23 16:39:42 -0500

    ArrayGamma, Sink

--- gamma/src/gamma/ArrayGamma.java
+++ gamma/src/gamma/ArrayGamma.java
@@ -1,8 +1,24 @@
 package gamma;
 
+import gammaSupport.Connector;
+
 /** Gamma HJOIN implementation lifted to arrays */
 public class ArrayGamma implements gammaSupport.GammaConstants {
-    
     // only graduate class has to implement this
-
+    
+    public ArrayGamma(Connector[] a, Connector[] b, Connector[] out, String hashkeyA, String hashkeyB) {       
+//        Connector[] bloomA_to_hjoin = Connector.newConnectorArray("bloomA to hjoin");
+//        Connector[] bloom_to_bfilter = Connector.newConnectorArray("bloom to bfilter");
+//        Connector[] bfilterB_to_hjoin = Connector.newConnectorArray("bfilterB to hjoin");
+//        
+//        for (int i = 0; i < splitLen; i ++) {
+//            new Bloom(a[i], bloomA_to_hjoin[i], bloom_to_bfilter[i], hashkeyA);
+//            new BFilter(b[i], bloom_to_bfilter[i], bfilterB_to_hjoin[i], hashkeyB);
+//            new HJoin(bloomA_to_hjoin[i], bfilterB_to_hjoin[i], out[i], hashkeyA, hashkeyB);
+//        }
+        
+        for (int i = 0; i < splitLen; i ++) {
+            new Gamma(a[i], b[i], out[i], hashkeyA, hashkeyB);
+        }
+    }
 }
--- gamma/src/gamma/Sink.java
+++ gamma/src/gamma/Sink.java
@@ -1,7 +1,30 @@
 package gamma;
 
+import PrologDB.Tuple;
+import gammaSupport.Connector;
+import gammaSupport.ReadEnd;
+import gammaSupport.ThreadList;
+
 /** sink box class -- read input connector until nothing is left to read;
  * do nothing otherwise.  useful for debugging and regression tests */
 public class Sink extends Thread implements gammaSupport.GammaConstants {
-  
+    ReadEnd in;
+    
+    @SuppressWarnings("LeakingThisInConstructor")
+    public Sink(Connector in) {
+        this.in = in.getReadEnd();
+        ThreadList.add(this);
+    }
+    
+    @Override
+    public void run() {
+        // to do  
+        while (true) {
+            Tuple t = in.getNextTuple();
+            if (t == null) {
+//                in.close();
+                break;
+            }
+        }
+    }
 }
