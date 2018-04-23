ff11abd8685e2cb1986436c45a1532a965980b1e
xinyuan <xinyuan.chn@gmail.com>
2018-04-23 10:09:21 -0500

    SplitM, MergeM

--- gamma/src/gamma/MergeM.java
+++ gamma/src/gamma/MergeM.java
@@ -1,6 +1,35 @@
 package gamma;
 
+import gammaSupport.BMap;
+import gammaSupport.Connector;
+import gammaSupport.ReadEnd;
+import gammaSupport.ThreadList;
+import gammaSupport.WriteEnd;
+
 /** Merge bitmap boxes into a single box -- check class notes!!! */
 public class MergeM extends Thread implements gammaSupport.GammaConstants {
-  
+    ReadEnd[] in;
+    WriteEnd out;
+
+    @SuppressWarnings("LeakingThisInConstructor")
+    public MergeM(Connector in[], Connector out) {
+        this.in = new ReadEnd[splitLen];
+        for (int i = 0; i < splitLen; i ++) { 
+            this.in[i] = in[i].getReadEnd(); 
+        }
+        
+        this.out = out.getWriteEnd();
+        
+        ThreadList.add(this);
+    }
+    
+    @Override
+    public void run() {
+        BMap bm = in[0].getNextBMap();
+        for (int i = 1; i < splitLen; i ++) {
+            bm = BMap.or(bm, in[i].getNextBMap());
+        }
+        out.putNextBMap(bm);
+        out.close();
+    }
 }
--- gamma/src/gamma/SplitM.java
+++ gamma/src/gamma/SplitM.java
@@ -1,7 +1,39 @@
 package gamma;
 
+import gammaSupport.BMap;
+import gammaSupport.Connector;
+import gammaSupport.ReadEnd;
+import gammaSupport.ThreadList;
+import gammaSupport.WriteEnd;
+
 /** split bit map  -- see class notes! */
 public class SplitM extends Thread implements gammaSupport.GammaConstants {
+    ReadEnd in;
+    WriteEnd[] out;
+    
+    @SuppressWarnings("LeakingThisInConstructor")
+    public SplitM(Connector in, Connector[] out) {
+        this.in = in.getReadEnd();
+        this.out = new WriteEnd[splitLen];
+        
+        for (int i = 0; i < splitLen; i ++) { 
+            this.out[i] = out[i].getWriteEnd(); 
+        }
 
- 
+        ThreadList.add(this);
+    }
+    
+    @Override
+    public void run() {
+        try {
+            BMap bm = new BMap(in.getSerializedMap());
+            in.close();
+            for (int i = 0; i < splitLen; i ++) {
+                out[i].putNextBMap(bm.extract(i)); 
+                out[i].close();
+            }
+        } catch (Exception e) {
+            System.out.println("ERROR! readEnd getSerializedMap failed!");
+        }
+    }
 }
